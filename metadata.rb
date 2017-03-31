48e91eb13c8bb4029b01e2851e2a176d9e5236bb
pcjoshi9 <pcjoshi9@gmail.com>
2017-03-31 11:08:19 -0700

    fixing metadata merge issue

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,7 @@ maintainer_email 'pcjoshi9@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures myfacts'
 long_description 'Installs/Configures myfacts'
-<<<<<<< HEAD
 version          '1.7.0'
-=======
-version          '1.6.0'
->>>>>>> 879eb948ad2ed12d4070f35071a3600a20c8357a
 issues_url       'localhost'
 source_url       'localhost'
 depends 'apt'
